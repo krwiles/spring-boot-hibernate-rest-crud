424be8cebea8c8fe15d5c6f508c94ad7d1f9b10f
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-14 18:22:47 -0400

    Refactors exception handling to global controller

--- 01-spring-boot-rest-crud/src/main/java/com/luv2code/restdemo/rest/StudentRestController.java
+++ 01-spring-boot-rest-crud/src/main/java/com/luv2code/restdemo/rest/StudentRestController.java
@@ -42,18 +42,4 @@ public class StudentRestController {
 
         return this.theStudents.get(studentId);
     }
-
-    // add exception handler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 }
