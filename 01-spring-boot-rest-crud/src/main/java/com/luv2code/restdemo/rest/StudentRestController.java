820a862d3cafc0a4c5427ca7b537b7cd554c5440
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-14 16:28:55 -0400

    Refactors StudentRestController

--- 01-spring-boot-rest-crud/src/main/java/com/luv2code/restdemo/rest/StudentRestController.java
+++ 01-spring-boot-rest-crud/src/main/java/com/luv2code/restdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.luv2code.restdemo.rest;
 
 import com.luv2code.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data (only called once)
+    @PostConstruct
+    public void loadData() {
+        this.theStudents = new ArrayList<>();
+
+        this.theStudents.add(new Student("Poornima", "Patel"));
+        this.theStudents.add(new Student("Mario", "Rossi"));
+        this.theStudents.add(new Student("Mary", "Smith"));
+    }
+
+
     // define endpoint for "/students" - return a list of students
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
-
-        theStudents.add(new Student("Poornima", "Patel"));
-        theStudents.add(new Student("Mario", "Rossi"));
-        theStudents.add(new Student("Mary", "Smith"));
-
         return theStudents;
     }
 }
