672c03cc8391156a5e7040fed7925e3b794a0042
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-14 22:21:50 -0400

    Adds additional DAO methods

--- 02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOImpl.java
+++ 02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOImpl.java
@@ -23,4 +23,25 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee dbEmployee = entityManager.merge(employee);
+        // has updated id in the case of an insert
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+
+        entityManager.remove(employee);
+    }
 }
